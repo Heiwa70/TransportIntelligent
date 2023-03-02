d69e1e373da4a25dd9bc1ab58cfb411ad3fd0604
clementseiler <clement70200@gmail.com>
2023-03-02 16:58:08 +0100

    Calcul distance mini

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 # This is a sample Python script.
+import itertools
+import math
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+tabville = [[1, 1], [2, 2], [1, 3], [3, 2], [3, 5], [7, 2]]
 
+def dist_villes(v1,v2):
+    return math.sqrt((v2[0] - v1[0])**2 + (v2[1] - v1[1])**2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def minimal_distance(points):
+    paths = list(itertools.permutations(points))
+    min_distance = float('inf')
+    for path in paths:
+        distance_total = 0
+        for i in range(len(path)-1):
+            distance_total += dist_villes(path[i], path[i+1])
+        if distance_total < min_distance:
+            min_distance = distance_total
+    return min_distance
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    min_dist = minimal_distance(tabville)
+    print(min_dist)

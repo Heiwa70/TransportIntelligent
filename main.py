43ec979a5b1cfb822cf580e4d20b5f74632f8a30
Clément <clement70200@gmail.com>
2023-03-15 14:06:01 +0100

    Plot

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # This is a sample Python script.
 import itertools
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 tabville = [[1, 1], [2, 2], [1, 3], [3, 2], [3, 5], [7, 2]]
-
+pts_x = [1,2,1,3,3,7]
+pts_y = [1,2,3,2,5,2]
 def dist_villes(v1,v2):
     return math.sqrt((v2[0] - v1[0])**2 + (v2[1] - v1[1])**2)
 
@@ -23,3 +27,7 @@ def minimal_distance(points):
 if __name__ == '__main__':
     min_dist = minimal_distance(tabville)
     print(min_dist)
+    ax = plt.subplot()
+    ax.axis([0,8,0,8])
+    ax.scatter(pts_x,pts_y)
+    plt.show()
